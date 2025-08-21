81c14d89d14cc68d1a3394a67ce2775de8f02c23
Fahadali470 <f9384470@gmail.com>
2025-08-21 23:17:21 +0500

    first app prep

---
+++ viteproject/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello world | Fahad Bhatti 
+      firstly we install git and make it comfortable with github 
+      then we open react documentation
+      then we create react app
+      and instal some pkgs though npm 
+      and then make changes in the src files
+      and remove the unusable files 
+      so thats our first project
+
+    </h1>
+  )
+}
+
+export default App
