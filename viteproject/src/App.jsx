0403048632e98368b3ab455a2174249391d2a358
Fahadali470 <f9384470@gmail.com>
2025-08-22 05:38:35 +0500

    practice

--- viteproject/src/App.jsx
+++ viteproject/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Chai from './Chai.jsx'
 
+  function App() {
   return (
-    <h1>Hello world | Fahad Bhatti 
-      firstly we install git and make it comfortable with github 
-      then we open react documentation
-      then we create react app
-      and instal some pkgs though npm 
-      and then make changes in the src files
-      and remove the unusable files 
-      so thats our first project
+  <>
+      <Chai/>
+      <h2>React Learning</h2>
+      <h3>vite</h3>
 
-    </h1>
-  )
+  </>  
+  
+)
 }
 
 export default App
